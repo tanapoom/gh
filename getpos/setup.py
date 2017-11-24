256efb4e773df0b2621ab94620b89e49962b7284
new555 <peeradon.jo.59@ubu.ac.th>
2017-11-24 14:27:26 +0700

    getposter

---
+++ getpos/setup.py
@@ -0,0 +1,48 @@
+"""
+getpos
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='getpos',
+    version='0.1.0',
+    url='https://github.com/tanapoom/getpos',
+    license='BSD',
+    author='tanapoom',
+    author_email='thanphum.su.57@ubu.ac.th',
+    description='getpos',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'getpos = getpos.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
